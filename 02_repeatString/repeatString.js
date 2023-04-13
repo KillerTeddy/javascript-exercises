55be0bc4659c2ae68e4f9d75168af8aee379dafc
KillerTeddy <alexkassabian@gmail.com>
2023-04-13 17:43:12 +1000

    Completed exercise 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(input, amount) {
+    if(amount < 0){
+        return 'ERROR'
+    }
+    if(input == ''){
+        return ''
+    }
+    
+    let repeatedString = ''
+    for (let i = 0; i < amount; i++){
+        repeatedString += input
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
