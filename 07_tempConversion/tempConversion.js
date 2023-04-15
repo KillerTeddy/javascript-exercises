aeb14f677de84c422778bbb492d21a3a1680da46
KillerTeddy <alexkassabian@gmail.com>
2023-04-15 15:56:24 +1000

    Completed tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureF) {
+  return parseFloat(((temperatureF - 32) * (5/9)).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureC) {
+  return parseFloat(((temperatureC * (9 / 5) + 32)).toFixed(1))
 };
 
 // Do not edit below this line
