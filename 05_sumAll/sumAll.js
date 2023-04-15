a23c12b37dcf5a98b96c7d05988608f8b36e1388
KillerTeddy <alexkassabian@gmail.com>
2023-04-15 15:48:15 +1000

    Completed exercise 4-6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR'
+    if (a < 0 || b < 0) return "ERROR"
+
+    let total = 0
+    let smallerNum = 0
+    let biggerNum = 0
+    if (a < b){ 
+        smallerNum = a
+        biggerNum = b
+    }
+    else {
+        smallerNum = b
+        biggerNum = a
+    }
+
+
+    for(let i = smallerNum; i < biggerNum + 1; i++){
+        total += i
+    }
+    return total
 
 };
 
